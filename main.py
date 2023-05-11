85283150f31326f5320127b46a50b4b89b873bf9
papertoy1127 <richard1909@naver.com>
2023-05-12 00:57:27 +0900

    clean code

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 import cv2
 import numpy as np
 import os
+import shutil
 
-class glyph_consonant:
+px_white = np.array([255, 255, 255, 255], dtype=np.uint8)
+px_black = np.array([0, 0, 0, 255], dtype=np.uint8)
+px_transparent = np.array([0, 0, 0, 0], dtype=np.uint8)
+
+consonants = 'ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ'
+vowels = 'ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ'
+consonant_pri = 'ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ'
+consonant_fin = ' ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ' # 맨 앞 공백은 받침 없음
+
+consonant_map = {
+    'ㄲ': ('ㄱ', 'ㄱ'),
+    'ㄸ':  ('ㄷ', 'ㄷ'),
+    'ㅃ': ('ㅂ', 'ㅂ'),
+    'ㅆ':  ('ㅅ', 'ㅅ'),
+    'ㄳ': ('ㄱ', 'ㅅ'),
+    'ㄵ':  ('ㄴ', 'ㅈ'),
+    'ㄶ': ('ㄴ', 'ㅎ'),
+    'ㄺ':  ('ㄹ', 'ㄱ'),
+    'ㄻ': ('ㄹ', 'ㅁ'),
+    'ㄺ':  ('ㄹ', 'ㄱ'),
+    'ㄼ': ('ㄹ', 'ㅂ'),
+    'ㄽ':  ('ㄹ', 'ㅅ'),
+    'ㄾ': ('ㄹ', 'ㅌ'),
+    'ㄿ':  ('ㄹ', 'ㅍ'),
+    'ㅀ': ('ㄹ', 'ㅎ'),
+    'ㅄ':  ('ㅂ', 'ㅅ'),
+    'ㅉ': ('ㅈ', 'ㅈ'),
+    ' ':  None
+}
+
+class GlyphConsonant:
     def __init__(self):
         # 초성
         self.pri_left = None
@@ -52,42 +83,85 @@ class glyph_consonant:
             return self.sec_left_up if fin else self.pri_left_up
 
 
-class glyph_vowel:
+class GlyphVowel:
     def __init__(self):
         self.primary = None
         self.secondary = None
-        self.fin_consonant_offset = 0
+        self.offset = 0
         self.group = 0
-
-def overwrite(base, img):
-    if type(img) == type(None):
-        return
-    bl = len(base)
-    for i in range(bl):
-        il = len(base[i])
-        for j in range(il):
-            pix = img[i, j]
-            if pix[3] == 255:
-                base[i, j] = np.array([255, 255, 255, 255], dtype=np.uint8)
-
-def loadimg(path, throw_exception = False):
-    if not os.path.exists(path): 
-        if not throw_exception:
-            return None
-        print(path)
-    stream = open(path, "rb")
-    bytes = bytearray(stream.read())
-    numpyarray = np.asarray(bytes, dtype=np.uint8)
-    return cv2.imdecode(numpyarray, cv2.IMREAD_UNCHANGED)
 
 def parse_hangul(chr):
     u = ord(chr) # unicode
     u = u - 0xAC00
-    fin = u % 28
-    sec = (u // 28) % 21
-    pri = u // 588
+    fin = consonant_fin[u % 28]
+    sec = vowels[(u // 28) % 21]
+    pri = consonant_pri[u // 588]
+
+    if pri in consonant_map:
+        pri = consonant_map[pri]
+    else:
+        pri = (pri)
+
+    if fin in consonant_map:
+        fin = consonant_map[fin]
+    else:
+        fin = (fin)
+
     return(pri, sec, fin, u)
 
+def load_consonant(letter):
+    n = consonants.find(letter)
+    cons = GlyphConsonant()
+    cons.pri_left = glyph_of(n, 0)
+    cons.pri_up = glyph_of(n, 1)
+    cons.pri_left_up = glyph_of(n, 2)
+    cons.sec_left = glyph_of(n, 3)
+    cons.sec_up = glyph_of(n, 4)
+    cons.sec_left_up = glyph_of(n, 5)
+    cons.fin = glyph_of(n, 6)
+    
+    cons.pri_left_double = glyph_of(n, 7)
+    cons.pri_up_double = glyph_of(n, 8)
+    cons.pri_left_up_double = glyph_of(n, 9)
+    cons.sec_left_double = glyph_of(n, 10)
+    cons.sec_up_double = glyph_of(n, 11)
+    cons.sec_left_up_double = glyph_of(n, 12)
+    cons.fin_double = glyph_of(n, 13)
+    return cons
+
+def load_vowel(letter):
+    global vowel_groups, vowel_offset
+    group = 0
+    if (letter in vowel_groups["Group_1"]): group = 1
+    elif (letter in vowel_groups["Group_2"]): group = 2
+    else: group = 3
+    offset = vowel_offset[letter]
+
+    vowl = GlyphVowel()
+    n = vowels.find(letter) + 14
+    vowl.primary = glyph_of(n, 0)
+    vowl.secondary = glyph_of(n, 1)
+
+    vowl.offset = offset
+    vowl.group = group
+    return vowl
+
+# Initialize glyphs
+glyph_img = cv2.imread("glyphs.png", cv2.IMREAD_GRAYSCALE)
+def glyph_of(x, y):
+    return glyph_img[y*17:y*17+16, x*17:x*17+16]
+
+def apply(base, glyph):
+    if glyph is None or len(glyph) == 0:
+        return
+    
+    for i in range(16):
+        for j in range(16):
+            if glyph[i,j] == 0:
+                base[i,j] = px_white
+    
+
+# Initalize vowel data
 vowel_groups = {}
 vowel_offset = {}
 
@@ -100,157 +174,58 @@ with open("vowel.txt", "r", encoding='utf-8') as v:
         else:
             vowel_offset[dat[0]] = int(dat[1])
 
+# Load jamo data
 
-def load_consonant(letter):
-    cons = glyph_consonant()
-    cons.pri_left = loadimg(f"assets/{letter}01.png")
-    cons.pri_up = loadimg(f"assets/{letter}02.png")
-    cons.pri_left_up = loadimg(f"assets/{letter}03.png")
-    cons.sec_left = loadimg(f"assets/{letter}05.png")
-    cons.sec_up = loadimg(f"assets/{letter}06.png")
-    cons.sec_left_up = loadimg(f"assets/{letter}07.png")
-    cons.fin = loadimg(f"assets/{letter}09.png")
-    
-    cons.pri_left_double = loadimg(f"assets/{letter}11.png")
-    cons.pri_up_double = loadimg(f"assets/{letter}12.png")
-    cons.pri_left_up_double = loadimg(f"assets/{letter}13.png")
-    cons.sec_left_double = loadimg(f"assets/{letter}15.png")
-    cons.sec_up_double = loadimg(f"assets/{letter}16.png")
-    cons.sec_left_up_double = loadimg(f"assets/{letter}17.png")
-    cons.fin_double = loadimg(f"assets/{letter}19.png")
-    return cons
-
-def load_vowel(letter):
-    global vowel_groups, vowel_offset
-    group = 0
-    if (letter in vowel_groups["Group_1"]): group = 1
-    elif (letter in vowel_groups["Group_2"]): group = 2
-    else: group = 3
-    offset = vowel_offset[letter]
-
-    vowl = glyph_vowel()
-    if (letter == 'ㅣ'): vowl.primary = loadimg(f"alternative/ㅣ01.png")
-    else: vowl.primary = loadimg(f"assets/{letter}0{group}.png")
-    vowl.secondary = loadimg(f"assets/{letter}0{group+3}.png")
-
-    vowl.fin_consonant_offset = offset
-    vowl.group = group
-    return vowl
-
-consonants = 'ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ'
-vowels = 'ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ'
-consonant_pri = 'ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ'
-consonant_fin = ' ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ' # 맨 앞 공백은 받침 없음
-
-glyphs_consonant = {}
-glyphs_vowel = {}
+glyph_consonant = {}
+glyph_vowel = {}
 
 for i in consonants:
-    glyphs_consonant[i] = load_consonant(i)
+    glyph_consonant[i] = load_consonant(i)
     
 for i in vowels:
-    glyphs_vowel[i] = load_vowel(i)
-
-def parse_consonant(con):
-    if con == 'ㄲ':
-        return('ㄱ', 'ㄱ')
-    if con == 'ㄸ':
-        return ('ㄷ', 'ㄷ')
-    if con == 'ㅃ':
-        return('ㅂ', 'ㅂ')
-    if con == 'ㅆ':
-        return ('ㅅ', 'ㅅ')
-    if con == 'ㅉ':
-        return('ㅈ', 'ㅈ')
-    
-    if con == 'ㄳ':
-        return('ㄱ', 'ㅅ')
-    if con == 'ㄵ':
-        return ('ㄴ', 'ㅈ')
-    if con == 'ㄶ':
-        return('ㄴ', 'ㅎ')
-    if con == 'ㄺ':
-        return ('ㄹ', 'ㄱ')
-    if con == 'ㄻ':
-        return('ㄹ', 'ㅁ')
-    if con == 'ㄺ':
-        return ('ㄹ', 'ㄱ')
-    if con == 'ㄼ':
-        return('ㄹ', 'ㅂ')
-    if con == 'ㄽ':
-        return ('ㄹ', 'ㅅ')
-    if con == 'ㄾ':
-        return('ㄹ', 'ㅌ')
-    if con == 'ㄿ':
-        return ('ㄹ', 'ㅍ')
-    if con == 'ㅀ':
-        return('ㄹ', 'ㅎ')
-    if con == 'ㅄ':
-        return ('ㅂ', 'ㅅ')
-    
-    if con == ' ':
-        return None
-    
-    return (con)
-
-def get_pri_img(con, group, fin):
-    if con == None: return None
-    if len(con) == 1:
-        gly = glyphs_consonant[con[0]]
-        return gly.get_img(group, fin)
-    if con[0] == con[1]:
-        gly = glyphs_consonant[con[0]]
-        return gly.get_img(group, fin, True)
-    '''
-    옛한글 지원시 코드 완성
-    gly1 = glyphs_consonant[con[0]]
-    img1 = gly1.get_img(group, fin, True)
-    gly2 = glyphs_consonant[con[1]]
-    img2 = gly2.get_img(group, fin, True)
-    [4, 0, 0, 0, 4, 9, 6]
-    '''
-
-def get_fin_img(con, offset):
-    if con == None: return None
-    img = None
-    if len(con) == 1:
-        gly = glyphs_consonant[con[0]]
-        img = gly.fin
-    elif con[0] == con[1]:
-        gly = glyphs_consonant[con[0]]
-        img = gly.fin_double
-    else:
-        gly1 = glyphs_consonant[con[0]]
-        img1 = np.zeros((16, 8, 4), dtype=np.uint8) if gly1.fin_double is None else gly1.fin_double
-        gly2 = glyphs_consonant[con[1]]
-        img2 = np.zeros((16, 8, 4), dtype=np.uint8) if gly2.fin_double is None else gly2.fin_double
-        img = np.concatenate((img1[:, :7], img2[:, 7:]), axis=1)
-
-    if offset == 0:
-        return img
-    return np.concatenate((np.zeros((16, offset, 4), dtype=np.uint8), img[:, :-offset]), axis=1)
-
+    glyph_vowel[i] = load_vowel(i)
 
 def get_image(letter):
     if 0xAC00 > ord(letter) or ord(letter) >= 0xD7A4:
         return np.zeros((16, 16, 4), dtype=np.uint8)
     p = parse_hangul(letter)
-    con = parse_consonant(consonant_pri[p[0]])
+    con = p[0]
     vow = p[1]
-    fin = parse_consonant(consonant_fin[p[2]])
+    fin = p[2]
 
-    gly_v = glyphs_vowel[vowels[vow]]
-    gly_f = None if fin == None else glyphs_consonant[fin[0]]
+    gly_v = glyph_vowel[vow]
 
-    img_p = get_pri_img(con, gly_v.group, gly_f != None)
-    img_v = gly_v.primary if gly_f == None else gly_v.secondary
-    img_f = get_fin_img(fin, gly_v.fin_consonant_offset)
+    img_p = None
+    if con is not None:
+        if len(con) == 1: # 단자음
+            img_p = glyph_consonant[con[0]].get_img(gly_v.group, fin)
+        elif con[0] == con[1]: # 쌍자음
+            img_p = glyph_consonant[con[0]].get_img(gly_v.group, fin, True)
+        
+        # 옛한글 지원시 합용병서 코드 추가
 
+    img_f = None
+    if fin is not None:
+        if len(fin) == 1: # 단자음
+            img_f = glyph_consonant[fin[0]].fin
+        elif fin[0] == fin[1]: # 각자 병서
+            img_f = glyph_consonant[fin[0]].fin_double
+        else: # 합용 병서
+            gly1 = glyph_consonant[fin[0]]
+            gly2 = glyph_consonant[fin[1]]
+            img1 = np.zeros((16, 8, 4), dtype=np.uint8) if gly1.fin_double is None else gly1.fin_double
+            img2 = np.zeros((16, 8, 4), dtype=np.uint8) if gly2.fin_double is None else gly2.fin_double
+            img_f = np.concatenate((img1[:, :7], img2[:, 7:]), axis=1)
+
+        if gly_v.offset > 0: # 모음에 따른 오프셋 조정
+            img_f = np.concatenate((np.full((16, gly_v.offset), 255, dtype=np.uint8), img_f[:, :-gly_v.offset]), axis=1)
+
+    img_v = gly_v.primary if fin is None else gly_v.secondary # 받침이 있는 모음과 없는 모음 구별
 
     base = np.zeros((16, 16, 4), dtype=np.uint8)
-    overwrite(base, img_p)
-    overwrite(base, img_v)
-    overwrite(base, img_f)
+    apply(base, img_p)
+    apply(base, img_v)
+    apply(base, img_f)
     return base
 
 def get_string(str):
@@ -261,16 +236,10 @@ def get_string(str):
     
     return np.concatenate(imgs, axis=1)
 
-    
-
-def save_img(txt, fn):
-    i = get_image(txt)
-    cv2.imwrite(f'{fn}.png', i)
-
-a = 0
-
-
 def get_all():
+    if os.path.exists('./font'):
+        shutil.rmtree('./font')
+    os.mkdir('./font')
     a = 0xAC00
     b = 0xAC
     x = 0xD7A4
@@ -286,16 +255,17 @@ def get_all():
             im1.append(img)
         img = np.concatenate(im1, axis=0)
         cv2.imwrite(f'font/unicode_page_{hex(b)[2:]}.png', img)
+        print(f'Generated Plane {hex(b)[2:].upper()}')
         b += 1
 
-get_all()
-            
-str = "종이"
-base1 = get_string("종이")
-base2 = get_string("폰트")
+if __name__ == '__main__':
+    #get_all()
+    base1 = get_string("픽세일")
+    base2 = np.concatenate((get_string("폰트"), np.zeros((16, 16, 4), dtype=np.uint8)), axis=1)
+    base3 = get_string("리소스")
 
-cv2.imwrite(f'thumbnail.png', np.concatenate((base1, base2), axis=0))
+    cv2.imwrite(f'thumbnail.png', np.concatenate((base1, base2, base3), axis=0))
 
-cv2.imshow("asdf", cv2.resize(base, (len(str) * 128, 128), interpolation=cv2.INTER_AREA))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow("asdf", cv2.resize(base2, (2 * 128, 128), interpolation=cv2.INTER_AREA))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
